49e0c6a2cfae5b24ac0a07fd5eb38994e0d2db6d
yarim <yairm@sonol.co.il>
2024-12-21 19:22:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './Components/Filter'
+import PersonForm from './Components/PersonForm'
+import Persons from './Components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: 'noName', number : 0}])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const useEffectHook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(useEffectHook, [])
+
+  const filteredPersons = newFilter.length === 0 ? persons : persons.filter((p) => p.name.includes(newFilter))
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+   if (persons.find((p) => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={newFilter} onChange={handleFilterChange} />
+      <PersonForm onSubmit={addPerson} name={newName} onNameChange={handleNameChange} number={newNumber} onNumberChange={handleNumberChange} />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
+    </div>
+  )
+}
+export default App
