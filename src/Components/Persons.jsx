54ddee7167143d73c9be1e3b8bf5164dc7e9a0f6
yarim <yairm@sonol.co.il>
2025-01-01 19:14:20 +0200

    Pb_2.14

--- src/Components/Persons.jsx
+++ src/Components/Persons.jsx
@@ -1,13 +1,16 @@
 import Person from './Person' 
 
-const Persons = ({persons}) => {
-    return (
-        <ul>
-        {persons.map((person, index) => 
-          <Person name={person.name} number={person.number} key={index}/>
-        )}
+const Persons = ({ persons, onDeletePerson }) => {
+  return (
+      <ul>
+          {persons.map((person, index) => (
+              <li key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
+                  <Person name={person.name} number={person.number} />
+                  <button style={{ marginLeft: '8px' }} onClick={() => onDeletePerson(person)}>Delete Me</button>
+              </li>
+          ))}
       </ul>
-    )
-}
+  );
+};
 
 export default Persons
--- src/Services/Persons.js
+++ src/Services/Persons.js
@@ -16,8 +16,14 @@ const update = (id, newObject) => {
     return request.then(response => response.data)
 }
 
+const deletePerson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(() =>getAll())
+}
+
 export default { 
   getAll: getAll, 
   create: create, 
-  update: update 
+  update: update,
+  deletePerson: deletePerson 
 }
